6b371caf8499e7d2baafc5b43a279c2fe8ef3a53
Jared Rhine <jared@wordzoo.com>
2020-02-07 01:47:27 -0800

    Full checki

--- homeapp/tests/unit/example.spec.ts
+++ homeapp/tests/unit/example.spec.ts
@@ -1,12 +1,12 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/hello-world.vue";
+import { shallowMount } from "@vue/test-utils"
+import HelloWorld from "@/components/hello-world.vue"
 
 describe("hello-world.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new message";
+    const msg = "new message"
     const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
-});
+      propsData: { msg },
+    })
+    expect(wrapper.text()).toMatch(msg)
+  })
+})
