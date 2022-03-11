15508a90be802cc404a03cc2f72bf5e4284c6f14
Jared Rhine <jared@wordzoo.com>
2022-03-10 17:50:55 -0800

    Finish dynamic theming and use it to change color scheme more sedate

--- homeapp/src/app.vue
+++ homeapp/src/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="jared-container" class="jared-container--alt">
+  <div id="jared-container" class="jared-container--alt" :style="jaredStyle">
     <div id="jared-sidebar">
       <header id="jared-sidebar--top">
         <a href="/">
@@ -31,6 +31,82 @@ import Sidebar from "./components/sidebar.vue"
 export default {
   name: "app",
   components: { Sidebar },
+  computed: {
+    jaredStyle: function () {
+      let style = {}
+      let theme = this.themes[this.currentTheme] || {}
+      const props = Object.getOwnPropertyNames(theme)
+      props.forEach((prop) => {
+        const styleAttr = "--jared--" + prop
+        const val = theme[prop]
+        if (val !== null) {
+          style[styleAttr] = val
+        }
+      })
+      return style
+    },
+    currentTheme: function () {
+      return "corporate" // TODO: make switchable
+    },
+    themes: function () {
+      const baseTheme = {
+        "font-base--size--smallphone": "12pt",
+        "font-base--size--tablet": "13pt",
+        "font-base--size--smalldesk": "13.5pt",
+        "font-base--size--widedesk": "14pt",
+
+        "color-grey--500": "#9e9e9e",
+        "color-grey--800": "#424242",
+        "color-grey--900": "#212121",
+
+        "color-alpha-h": "140",
+        "color-alpha-s": "60%",
+        "color-alpha-l": "25%",
+        "color-beta-h": "60",
+        "color-beta-s": "85%",
+        "color-beta-l": "80%",
+
+        "color-alpha--light10":
+          "hsl(var(--jared--color-alpha-h), var(--jared--color-alpha-s), calc(var(--jared--color-alpha-l) + 10%))",
+        "color-alpha--light15":
+          "hsl(var(--jared--color-alpha-h), var(--jared--color-alpha-s), calc(var(--jared--color-alpha-l) + 15%))",
+        "color-alpha--dark5":
+          "hsl(var(--jared--color-alpha-h), var(--jared--color-alpha-s), calc(var(--jared--color-alpha-l) - 5%))",
+        "color-alpha--dark10":
+          "hsl(var(--jared--color-alpha-h), var(--jared--color-alpha-s), calc(var(--jared--color-alpha-l) - 10%))",
+        "color-alpha--dark15":
+          "hsl(var(--jared--color-alpha-h), var(--jared--color-alpha-s), calc(var(--jared--color-alpha-l) - 15%))",
+        "color-beta--light10":
+          "hsl(var(--jared--color-beta-h), var(--jared--color-beta-s), calc(var(--jared--color-beta-l) + 10%))",
+        "color-beta--dark45":
+          "hsl(var(--jared--color-beta-h), var(--jared--color-beta-s), calc(var(--jared--color-beta-l) - 45%))",
+
+        "color-alpha":
+          "hsl(var(--jared--color-alpha-h), var(--jared--color-alpha-s), var(--jared--color-alpha-l))",
+        "color-beta":
+          "hsl(var(--jared--color-beta-h), var(--jared--color-beta-s), var(--jared--color-beta-l))",
+
+        "link-underline-color": "var(--jared--color-beta--dark45)",
+
+        "text-color": "var(--jared--color-beta)",
+      }
+
+      return {
+        jaredGreen: {
+          ...baseTheme,
+        },
+        corporate: {
+          ...baseTheme,
+          "color-alpha-h": "208",
+          "color-alpha-s": "85%",
+          "color-alpha-l": "7%",
+          "color-beta-h": "34",
+          "color-beta-s": "78%",
+          "color-beta-l": "84%",
+        },
+      }
+    },
+  },
 }
 </script>
 
@@ -39,44 +115,9 @@ export default {
 :root {
   --jared--font-modernize-default: system-ui,-apple-system,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji';
   --jared--font-jared-temp: "Avenir", Helvetica, Arial, sans-serif;
-  // --jared--font-markdown: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
   --jared--font-markdown: Helvetica,Arial,sans-serif;
   --jared--font-mono: Consolas, "Andale Mono WT", "Andale Mono", "Lucida Console", "Lucida Sans Typewriter", "DejaVu Sans Mono", "Bitstream Vera Sans Mono", "Liberation Mono", "Nimbus Mono L", Menlo, Monaco, "Courier New", Courier, monospace;
   --jared--font-base: var(--jared--font-modernize-default);
-  // --jared--font-base: var(--jared--font-mono);
-  // --jared--font-base: var(--jared--font-markdown);
-  // --jared--font-base: var(--jared--font-jared-temp);
-
-  /* media queries not possible
-  --jared--breakpoint--tablet: 768px;
-  --jared--breakpoint--smalldesk: 1024px;
-  --jared--breakpoint--widedesk: 1280px;
-  */
-
-  --jared--font-base--size--smallphone: 12pt;
-  --jared--font-base--size--tablet: 13pt;
-  --jared--font-base--size--smalldesk: 13.5pt;
-  --jared--font-base--size--widedesk: 14pt;
-
-  --jared--color-alpha-h: 140;
-  --jared--color-alpha-s: 60%;
-  --jared--color-alpha-l: 25%;
-  --jared--color-alpha: hsl(var(--jared--color-alpha-h), var(--jared--color-alpha-s), var(--jared--color-alpha-l));
-  --jared--color-alpha--light10: hsl(var(--jared--color-alpha-h), var(--jared--color-alpha-s), calc(var(--jared--color-alpha-l) + 10%));
-  --jared--color-alpha--light15: hsl(var(--jared--color-alpha-h), var(--jared--color-alpha-s), calc(var(--jared--color-alpha-l) + 15%));
-  --jared--color-alpha--dark5: hsl(var(--jared--color-alpha-h), var(--jared--color-alpha-s), calc(var(--jared--color-alpha-l) - 5%));
-  --jared--color-alpha--dark10: hsl(var(--jared--color-alpha-h), var(--jared--color-alpha-s), calc(var(--jared--color-alpha-l) - 10%));
-  --jared--color-alpha--dark15: hsl(var(--jared--color-alpha-h), var(--jared--color-alpha-s), calc(var(--jared--color-alpha-l) - 15%));
-  --jared--color-beta-h: 60;
-  --jared--color-beta-s: 85%;
-  --jared--color-beta-l: 80%;
-  --jared--color-beta: hsl(var(--jared--color-beta-h), var(--jared--color-beta-s), var(--jared--color-beta-l));
-  --jared--color-beta--light10: hsl(var(--jared--color-beta-h), var(--jared--color-beta-s), calc(var(--jared--color-beta-l) + 10%));
-  --jared--color-beta--dark45: hsl(var(--jared--color-beta-h), var(--jared--color-beta-s), calc(var(--jared--color-beta-l) - 45%));
-  --jared--color-grey--50: hsl(0,0%,50%);
-  --jared--color-grey--80: hsl(0,0%,73.4%); /* #bbb */
-  --jared--text-color: var(--jared--color-beta);
-  --jared--link-underline-color: var(--jared--color-beta--dark45);
 
   --jared--spacing--2xs: 0.125rem;
   --jared--spacing--xs: 0.20rem;
@@ -100,8 +141,6 @@ export default {
 
 body {
   font-family: var(--jared--font-base);
-  background-color: var(--jared--color-alpha);
-  color: var(--jared--text-color);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
@@ -188,6 +227,9 @@ body {
     --jared-sidebar-width: 9.5rem;
     line-height: 1.3rem;
 
+    background-color: var(--jared--color-alpha);
+    color: var(--jared--text-color);
+
     display: grid;
     grid-template-rows: auto 1fr;
     grid-template-areas: "sidebar" "content";
@@ -195,7 +237,8 @@ body {
 
   #jared-content,
   #jared-sidebar {
-    padding: 0.6rem;
+    min-height: 100vh;
+    padding: var(--jared--spacing--lg);
   }
 
   #jared-content {
@@ -256,13 +299,12 @@ body {
 @media screen and (min-width: 600px) {
   #jared-container {
     grid-template-rows: none;
-    grid-template-columns: var(--jared-sidebar-width) minmax(300px, 1100px); // 1fr; // minmax(1fr, 1200px); // auto 1fr; // can't auto in col 1?
+    grid-template-columns: var(--jared-sidebar-width) minmax(300px, 900px); // 1fr; // minmax(1fr, 1200px); // auto 1fr; // can't auto in col 1?
     grid-template-areas: "sidebar content sidebar-bottom";
   }
 
   #jared-sidebar {
     width: var(--jared-sidebar-width);
-    height: 100vh;
     position: fixed;
     grid-template-rows: auto 1fr auto;
     grid-template-columns: auto;
